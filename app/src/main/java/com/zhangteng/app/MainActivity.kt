aee31d6992351e167b0c77ad88bf7c1dddbb5c11
Swing <763263311@qq.com>
2022-09-14 23:35:20 +0800

    删除无用代码

--- app/src/main/java/com/zhangteng/app/MainActivity.kt
+++ app/src/main/java/com/zhangteng/app/MainActivity.kt
@@ -3,62 +3,29 @@ package com.zhangteng.app
 import android.content.DialogInterface
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
 import com.zhangteng.aop.annotation.CheckNet
 import com.zhangteng.aop.annotation.Permissions
 import com.zhangteng.aop.annotation.SingleClick
 import com.zhangteng.aop.annotation.TimeLog
 import com.zhangteng.app.activity.*
-import com.zhangteng.app.mvp.model.imodel.IMainModel
-import com.zhangteng.app.mvp.presenter.MainPresenter
-import com.zhangteng.app.mvp.presenter.ipresenter.IMainPresenter
-import com.zhangteng.app.mvp.view.IMainView
 import com.zhangteng.app.ui.mvvm.MvvmActivity
 import com.zhangteng.app.ui.mvvmdb.MvvmDbActivity
-import com.zhangteng.mvp.mvp.BaseMvpActivity
-import com.zhangteng.mvp.utils.LoadingPresenterHandler
+import com.zhangteng.base.base.BaseActivity
 import com.zhangteng.utils.StateViewHelper
 import com.zhangteng.utils.jumpToActivity
-import java.lang.reflect.Proxy
 
-class MainActivity : BaseMvpActivity<IMainView, IMainModel, IMainPresenter>(), IMainView {
-    private var tv_TextView: TextView? = null
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    override fun createPresenter(): IMainPresenter? {
-//        return MainPresenter()
-        return Proxy.newProxyInstance(
-            MainPresenter::class.java.classLoader,
-            arrayOf(IMainPresenter::class.java),
-            LoadingPresenterHandler(MainPresenter())
-        ) as IMainPresenter
-    }
-
     override fun initView() {
-        super.initView()
-        tv_TextView = findViewById(R.id.tv_TextView)
+
     }
 
     override fun initData() {
-        showToast(mPresenter?.testString())
-    }
-
-    override fun showLoadingView() {
-        showProgressDialog()
-//        showNoNetView(tv_TextView)
-//        showEmptyView(tv_TextView)
-    }
-
-    override fun dismissLoadingView() {
-        dismissProgressDialog()
-//        showContentView(tv_TextView)
-    }
-
-    override fun inflateView(data: String?) {
 
     }
 
