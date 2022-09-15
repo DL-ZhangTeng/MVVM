abe043f21867e05700f7c9868c0826c0ffedf205
Swing <763263311@qq.comm>
2022-09-15 10:50:52 +0800

    使用gradle7.3.3

--- app/src/main/java/com/zhangteng/app/MainActivity.kt
+++ app/src/main/java/com/zhangteng/app/MainActivity.kt
@@ -3,10 +3,6 @@ package com.zhangteng.app
 import android.content.DialogInterface
 import android.os.Bundle
 import android.view.View
-import com.zhangteng.aop.annotation.CheckNet
-import com.zhangteng.aop.annotation.Permissions
-import com.zhangteng.aop.annotation.SingleClick
-import com.zhangteng.aop.annotation.TimeLog
 import com.zhangteng.app.activity.*
 import com.zhangteng.app.ui.mvvm.MvvmActivity
 import com.zhangteng.app.ui.mvvmdb.MvvmDbActivity
@@ -29,45 +25,34 @@ class MainActivity : BaseActivity() {
 
     }
 
-    @TimeLog
-    @CheckNet
-    @SingleClick
-    @Permissions(value = ["android.permission.WRITE_EXTERNAL_STORAGE", "android.permission.READ_EXTERNAL_STORAGE"])
     fun onClickBaseLayout(v: View) {
         jumpToActivity<BaseDemoActivity>()
     }
 
-    @TimeLog
     fun onClickTabLayout(v: View) {
         jumpToActivity<TabLayoutActivity>()
     }
 
-    @TimeLog
     fun onClickTree(v: View) {
         jumpToActivity<TreeActivity>()
     }
 
-    @TimeLog
     fun onClickNineImage(v: View) {
         jumpToActivity<NineImageActivity>()
     }
 
-    @TimeLog
     fun onClickMvvm(v: View) {
         jumpToActivity<MvvmActivity>()
     }
 
-    @TimeLog
     fun onClickMvvmDb(v: View) {
         jumpToActivity<MvvmDbActivity>()
     }
 
-    @TimeLog
     fun onClickListMvvm(v: View) {
         jumpToActivity<BaseListMvvmDemoActivity>()
     }
 
-    @TimeLog
     fun onClickListMvvmDb(v: View) {
         jumpToActivity<BaseListMvvmDbDemoDbActivity>()
     }
