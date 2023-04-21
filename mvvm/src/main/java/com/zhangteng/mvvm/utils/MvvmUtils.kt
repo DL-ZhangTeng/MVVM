87802b5a9a2a8a9235992cff3883987f2fe2e30a
Swing <763263311@qq.com>
2023-04-22 00:50:17 +0800

    ViewModel类获取优化

--- mvvm/src/main/java/com/zhangteng/mvvm/utils/MvvmUtils.kt
+++ mvvm/src/main/java/com/zhangteng/mvvm/utils/MvvmUtils.kt
@@ -1,11 +1,39 @@
 package com.zhangteng.mvvm.utils
 
+import androidx.lifecycle.ViewModel
 import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
 
 /**
  * 获取当前类绑定的泛型ViewModel-clazz
  */
 @Suppress("UNCHECKED_CAST")
 fun <VM> getVmClazz(obj: Any): VM {
-    return (obj.javaClass.genericSuperclass as ParameterizedType).actualTypeArguments[0] as VM
+    var type: VM? = null
+    (obj.javaClass.genericSuperclass as ParameterizedType).actualTypeArguments.forEach {
+        if (isVm(it)) {
+            type = it as VM
+            return@forEach
+        }
+    }
+    return type!!
+}
+
+fun isVm(type: Type?): Boolean {
+    if (type is Class<*>?) {
+        return when (type) {
+            ViewModel::class.java -> {
+                true
+            }
+
+            null -> {
+                false
+            }
+
+            else -> {
+                isVm(type.superclass)
+            }
+        }
+    }
+    return false
 }
